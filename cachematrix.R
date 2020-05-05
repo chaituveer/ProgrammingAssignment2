8fa28c5c5f35f1b99195e1cd06f6bbdae805a66a
chaituveer <chaithanyaveeraiahgari@gmail.com>
2020-05-05 14:57:59 -0500

    Create lexical scoping for cache matrix and get the cache solving

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## to create lexical looping functions
+## functions create a cache matrix and get the cache by sloving the matrix
 
-## Write a short comment describing this function
+## creating a cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ins <- NULL
+        set <- function(y) {
+                x <<- y
+                ins <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) ins <<- solve
+        getsolve <- function() ins
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+        
 }
 
 
-## Write a short comment describing this function
+## cache solving matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ins <- x$getsolve()
+        if(!is.null(ins)) {
+                message("getting inversed matrix")
+                return(ins)
+        }
+        data <- x$get()
+        ins <- solve(data, ...)
+        x$setsolve(ins)
+        ins
 }
